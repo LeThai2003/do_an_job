eee3236e85049463de9715e22f54781e5805f07a
Le Duc Thai <leducthai1008@gmail.com>
2024-05-08 18:15:12 +0700

    Cập nhật trang thông tin công ty

--- controller/admin/info-company.controller.js
+++ controller/admin/info-company.controller.js
@@ -1,4 +1,5 @@
 const CompanyModel = require("../../models/Company.model");
+const he = require('he');
 
 //[GET] //manage/infoCompany/:congTyId
 module.exports.index = async(req, res) => {
@@ -20,10 +21,39 @@ module.exports.edit = async(req, res) => {
     const congTyId = req.params.congTyId;
     const company = await CompanyModel.getCompanyById(congTyId);
 
-    console.log(company)
-
     res.render("admin/pages/infoCompany/edit", {
         title: "Trang chỉnh sửa thông tin công ty",
         company
     })
+}
+
+//[POST] //manage/infoCompany/:congTyId/edit
+module.exports.editPost = async(req, res) => {
+    try {
+        const congTyId = req.params.congTyId;
+
+        req.body.moTa = he.decode(req.body.moTa);
+        req.body.quyMo = parseInt(req.body.quyMo);
+
+        let query = ``;
+
+        const infoCompany = req.body;
+
+        if(req.file && req.file.filename)
+        {
+            infoCompany.logo = `/uploads/${req.file.filename}`;
+            query = `update CONGTY set tenCT = N'${infoCompany.tenCT}', diaDiem = N'${infoCompany.diaDiem}', sdtCT = '${infoCompany.sdtCT}', emailCT = '${infoCompany.emailCT}', quyMo = ${infoCompany.quyMo}, moTa = N'${infoCompany.moTa}', logo = N'${infoCompany.logo}' where congTyId = ${congTyId}`;
+        }
+        else{
+            query = `update CONGTY set tenCT = N'${infoCompany.tenCT}', diaDiem = N'${infoCompany.diaDiem}', sdtCT = '${infoCompany.sdtCT}', emailCT = '${infoCompany.emailCT}', quyMo = ${infoCompany.quyMo}, moTa = N'${infoCompany.moTa}' where congTyId = ${congTyId}`;
+        }
+
+        await CompanyModel.updateInfoCompany(query);
+
+        res.redirect("back")
+    } catch (error) {
+        console.log(error);
+        console.log("Lỗi trang cập nhật thông tin công ty - manage")
+        res.redirect("/");
+    }
 }
--- routes/admin/info-company.route.js
+++ routes/admin/info-company.route.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controller/admin/info-company.controller");
+const multer  = require('multer')
+
+const storageMulter = require("../../helpers/storage-multer.helper");
+  
+const upload = multer({ storage: storageMulter() })
 
 router.get("/:congTyId", controller.index);
 
 router.get("/:congTyId/edit", controller.edit);
+
+router.post("/:congTyId/edit", upload.single('logo'), controller.editPost);
 
 
 module.exports = router;
